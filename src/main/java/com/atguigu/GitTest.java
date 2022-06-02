c1d14145f982f562b50e7808ce646445b708791b
SenSen <840473030@qq.com>
2022-06-02 09:30:00 +0800

    第二次更新

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("第一次更新");
+        System.out.println("第二次更新");
     }
 }
